f93ea65481756705417834e469997252eaaeb979
JonCTack <jon.c.tackett@gmail.com>
2022-11-18 15:21:30 -0600

    second commit

---
+++ 1900sInvent.js
@@ -0,0 +1,24 @@
+let i=1900
+while(i<1950){
+
+    if(i==1900){
+        console.log(`it is ${i} - the zeppelin is invented!`)
+    }else if(i==1902){
+        console.log(`it is ${i} - the teddy bear is invented!`)
+    }else if(i==1910){
+        console.log(`it is ${i} - the talking motion picture is invented!`)
+    }else if(i==1913){
+        console.log(`it is ${i} - the bra is invented!`)
+    }else if(i==1918){
+        console.log(`it is ${i} - the fortune cookie is invented!`)
+    }else if(i==1923){
+        console.log(`it is ${i} - the traffic signal is invented!`)
+    }else if(i==1935){
+        console.log(`it is ${i} - the radar is invented!`)
+    }else if(i==1938){
+        console.log(`it is ${i} - the ballpoint pen is invented!`)
+    }else if(i==1943){
+        console.log(`it is ${i} - the slinky is invented!`)
+    }
+i++
+}
---
+++ ageCheck.js
@@ -0,0 +1,8 @@
+var age = 57
+for (i=0; i<=100; i++){
+    if(i == age){
+        console.log("That's the age!")
+    } else{
+        console.log("age doesn't match")
+    }
+}
